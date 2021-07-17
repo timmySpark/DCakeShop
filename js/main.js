c509d2cbbc0079c15ee30e68a68b4dcce3943086
timmySpark <84716345+timmySpark@users.noreply.github.com>
2021-07-17 05:07:17 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -96,7 +96,7 @@ window.addEventListener('DOMContentLoaded', event => {
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
-	    Activate Bootstrap scrollspy on the main nav element
+	   // Activate Bootstrap scrollspy on the main nav element
 	    const mainNav = document.body.querySelector('#mainNav');
 	    if (mainNav) {
 	        new bootstrap.ScrollSpy(document.body, {
